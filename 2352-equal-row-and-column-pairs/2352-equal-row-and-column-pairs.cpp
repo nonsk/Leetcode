ea092196ef68059894ec56a373e03c0466a148a7
nonsk <132883762+nonsk@users.noreply.github.com>
2024-09-28 15:51:06 +0530

    Time: 764 ms (5%), Space: 574 MB (5.05%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int equalPairs(vector<vector<int>>& grid) {
         unordered_map<string, int> umap;
-
+        int n = grid.size();
         for (auto it : grid) {
             string s = "";
             for (int i = 0; i < it.size(); i++) {
@@ -12,9 +12,9 @@ public:
             umap[s]++;
         }
         int count = 0;
-        for (int j = 0; j < grid.size(); j++) {
+        for (int j = 0; j < n; j++) {
             string s = "";
-            for (int i = 0; i < grid[0].size(); i++) {
+            for (int i = 0; i < n; i++) {
                 s = s + to_string(grid[i][j]);
                 s += ",";
             }
