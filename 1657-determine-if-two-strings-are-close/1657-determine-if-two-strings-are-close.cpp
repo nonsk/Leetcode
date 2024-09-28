d7209995387de35a55fb5b9bf49116291bccc164
nonsk <132883762+nonsk@users.noreply.github.com>
2024-09-28 16:46:33 +0530

    Time: 104 ms (40.95%), Space: 22.3 MB (59.24%) - LeetHub

--- 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
+++ 1657-determine-if-two-strings-are-close/1657-determine-if-two-strings-are-close.cpp
@@ -1,30 +1,37 @@
 class Solution {
 public:
     bool closeStrings(string word1, string word2) {
-        map<char, int> alpha;
-        vector<int> num;
+        // map<char, int> alpha;
+        set <char> alpha;
+        vector<int> num(26, 0);
         for (auto it : word1) {
-            alpha[it]++;
+            alpha.insert(it);
+            num[it-'a']++;
         }
-        for (auto it : alpha) {
-            num.push_back(it.second);
-        }
-        map<char, int> alpha2;
-        vector<int> num2;
+        // for (auto it : alpha) {
+        //     num.push_back(it.second);
+        // }
+        // map<char, int> alpha2;
+        set <char> alpha2;
+        vector<int> num2(26,0);
         for (auto it : word2) {
-            alpha2[it]++;
+            alpha2.insert(it);
+            num2[it-'a']++;
         }
-        for (auto it : alpha2) {
-            num2.push_back(it.second);
-        }
+        // for (auto it : alpha2) {
+        //     num2.push_back(it.second);
+        // }
         sort(num.begin(), num.end());
         sort(num2.begin(), num2.end());
         bool flag1 = true;
         bool flag2 = true;
-        for(auto it: alpha){
-            if(alpha2.find(it.first)==alpha2.end()){
-                flag1 = false;
-            }
+        // for(auto it: alpha){
+        //     if(alpha2.find(it.first)==alpha2.end()){
+        //         flag1 = false;
+        //     }
+        // }
+        if(alpha != alpha2){
+            flag1 = false;
         }
         
         if (num != num2) {
