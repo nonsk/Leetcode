04376d6caf89e8dfe8be4bb29a149571eaeb0ae1
nonsk <132883762+nonsk@users.noreply.github.com>
2024-09-29 15:29:21 +0530

    Time: 4 ms (43.8%), Space: 15.4 MB (45.31%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
         void solve(string str, int k, int c, vector<string> &ans, int n){
             if(c>k||c>n){
-                cout<<str<<endl;
                 return;
             }
             if(c==n){
-                cout<<str<<endl;
-                //store
                 ans.push_back(str);
                 return;
             }
-            cout<<str<<endl;
             if(k<n){solve(str+'(', k+1, c, ans,n);}
             if(c<n){solve(str+')', k, c+1, ans,n);}
-
             return;
-
-
         }
     vector<string> generateParenthesis(int n) {
         vector<string> ans;
