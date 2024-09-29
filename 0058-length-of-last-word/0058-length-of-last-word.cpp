a8b50d1500a436d676ea863ac56bbad31a83aedd
nonsk <132883762+nonsk@users.noreply.github.com>
2024-09-29 17:27:06 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        int n = s.size();
+        while(s[s.size()-1]==' ') {s.pop_back();}
+        if(s.size()==1){return 1;}
+        n = s.size();
+        while(s[n-count-1]!=' '){
+            count++;
+        }
+        return count;
+    }
+};
