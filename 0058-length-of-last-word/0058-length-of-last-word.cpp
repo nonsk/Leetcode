db2389983d116f17eb0b99d90437e9a46f3bf807
nonsk <132883762+nonsk@users.noreply.github.com>
2024-09-29 17:28:20 +0530

    Time: 4 ms (47.32%), Space: 8 MB (66.48%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.cpp
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -2,11 +2,11 @@ class Solution {
 public:
     int lengthOfLastWord(string s) {
         int count=0;
-        // int n = s.size();
+        int n = s.size();
         while(s[s.size()-1]==' ') {s.pop_back();}
         if(s.size()==1){return 1;}
-        // n = s.size();
-        while(count!=s.size()&&s[s.size()-count-1]!=' '){
+        n = s.size();
+        while(count!=n&&s[n-count-1]!=' '){
             count++;
         }
         return count;
