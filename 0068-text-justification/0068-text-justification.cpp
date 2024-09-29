8307caa9e28c7f29837cb1e42a5a5f7930a35fb7
nonsk <132883762+nonsk@users.noreply.github.com>
2024-09-30 01:00:34 +0530

    Time: 6 ms (12.95%), Space: 9.4 MB (57.94%) - LeetHub

--- 0068-text-justification/0068-text-justification.cpp
+++ 0068-text-justification/0068-text-justification.cpp
@@ -1,83 +1,59 @@
 class Solution {
 public:
     vector<string> fullJustify(vector<string>& words, int maxWidth) {
-        string s;
-        vector<string> answer;
-        string line = "";
-        int char_sum = 0;
-        int num_of_words = 0;
-        vector<string> temp_words;
-        int i;
-        for(i = 0; i < words.size(); i++) {
-            temp_words.push_back(words[i]);
-            num_of_words = temp_words.size();
-            char_sum = char_sum + words[i].size();
-            if (char_sum + num_of_words - 1 > maxWidth) {
-                while (char_sum + num_of_words - 1 > maxWidth) {
-                    temp_words.pop_back();
-                    char_sum = char_sum - words[i].size();
-                    i--;
+        vector<string> resTotal;
+        for (int i = 0; i < words.size(); i++) {
+            vector<string> tempRow;
+            int currentSize = 0;
+            int j = i;
+            for (j = i; j < words.size(); j++) {
+                if (currentSize + words[j].size() <= maxWidth) {
+                    tempRow.push_back(words[j]);
+                    currentSize += (words[j].size() + 1);
+                } else
+                    break;
+            }
+
+            string res = "";
+            int leftOverSpace = maxWidth - currentSize + 1;
+            int distribute = 0, left = 0;
+
+            if (tempRow.size() > 1) {
+                distribute = leftOverSpace / (tempRow.size() - 1);
+                left = leftOverSpace % (tempRow.size() - 1);
+            } else {
+                distribute = leftOverSpace;
+            }
+            if (j == words.size() || tempRow.size() == 1) {
+                for (int i = 0; i < tempRow.size() - 1; i++) {
+                    res += tempRow[i];
+                    res += " ";
                 }
-                line = "";
-                for (int it = 0; it < temp_words.size(); it++) {
-                    
-                    if (it == temp_words.size() - 1) {
-                            
-                        if(line.size() + temp_words[it].size() < maxWidth) {
-                            if (maxWidth - line.size() - temp_words[it].size()<
-                                temp_words.size() - 1) {
-                                line = "";
-                                int diff = maxWidth - line.size() - temp_words[it].size();
-                                           cout<<"initial : "<<diff<<endl;
-                                for (int it1 = 0; it1 < temp_words.size()-1;
-                                     it1++) {
-                                    if (diff == 0) {
-                                        line = line + temp_words[it1];
-                                    } else {
-                                        cout<<"for"<<line<<". "<<diff<<endl;
-                                        line = line + temp_words[it1] + ' ';
-                                        diff--;
-                                    }
-                                }
-                                line += temp_words[it];
-
-                                break;
-                            }
-                            line = "";
-                            it = -1;
-                        }
-                        else if (line.size() + temp_words[it].size() == maxWidth) {
-                            line = line + temp_words[it];
-                            break;
+                res += tempRow.back();
+                for (int i = 0; i < leftOverSpace; i++) {
+                    res += " ";
+                }
+            } else {
+                for (int i = 0; i < tempRow.size(); i++) {
+                    res += tempRow[i];
+                    if (i != tempRow.size() - 1) {
+                        res += " ";
+                        for (int j = 0; j < distribute; j++) {
+                            res += " ";
                         }
 
-                    } 
-                    else {
-                        temp_words[it] += ' ';
-                        line += temp_words[it];
+                        if (left > 0) {
+                            res += " ";
+                            left--;
+                        }
                     }
                 }
-                answer.push_back(line);
-                line = "";
-                temp_words.clear();
-                char_sum = 0;
+                cout << res << endl;
             }
 
+            resTotal.push_back(res);
+            i = j - 1;
         }
-        cout<<temp_words[0]<<endl;
-        string last_line = "";
-        for(int next = 0; next<=temp_words.size(); next++){
-            if(next==temp_words.size()){       
-                string latest_space(maxWidth-last_line.size(), ' ');
-                last_line += latest_space;
-                break;
-            }
-            last_line += temp_words[next]+' ';
-            cout<<next<<endl<<temp_words[next]<<endl<<last_line<<endl;
-
-        }
-        answer.push_back(last_line);
-
-        return answer;
+        return resTotal;
     }
 };
