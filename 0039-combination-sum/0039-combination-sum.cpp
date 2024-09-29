2034809085a609e93bae063004b0397c9b59887c
nonsk <132883762+nonsk@users.noreply.github.com>
2024-09-29 15:56:33 +0530

    Time: 88 ms (7.5%), Space: 68.6 MB (8.59%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    void solve(int index, int sum, int target, vector<int>& candidates,
+               vector<vector<int>>& ans, vector<int> curr) {
+        if (sum > target) {
+            return;
+        }
+        if (index >= candidates.size()) {
+            return;
+        }
+        if (sum == target) {
+            ans.push_back(curr);
+            return;
+        }
+        
+        solve(index + 1, sum, target, candidates, ans, curr);
+        curr.push_back(candidates[index]);
+        solve(index, sum + candidates[index], target, candidates, ans, curr);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> curr;
+        solve(0, 0, target, candidates, ans, curr);
+        return ans;
+    }
+};
