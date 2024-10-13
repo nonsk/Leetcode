60c9259838dbfb816fa1c7d8b1d86fbdfd4db8f5
nonsk <132883762+nonsk@users.noreply.github.com>
2024-10-13 19:26:48 +0530

    Time: 214 ms (15.56%), Space: 120.2 MB (5.47%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -6,18 +6,11 @@ public:
             mp[nums[i]].push_back(i);
         }
         for (auto it : mp) {
-            cout << it.first << "->";
-            for (auto it2 : it.second) {
-                cout << it2 << " ";
-            }
-            cout << endl;
-        }
-        for (auto it : mp) {
             for (int i = 0; i < it.second.size(); i++) {
                 for (int j = i+1; j < it.second.size(); j++) {
-                    cout<<abs(it.second[i]-it.second[j])<<endl;
+                    
                     if (abs(it.second[i] - it.second[j]) <= k) {
-                        cout<<"yes";
+                        
                         return true;
                     }
                 }
