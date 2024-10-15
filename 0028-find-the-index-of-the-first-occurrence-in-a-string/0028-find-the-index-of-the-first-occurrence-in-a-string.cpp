2a91aeee30f8853b73d547a7dc6073775931a45a
nonsk <132883762+nonsk@users.noreply.github.com>
2024-10-15 23:29:26 +0530

    Time: 0 ms (100%), Space: 7.8 MB (65.28%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,24 +1,50 @@
 class Solution {
 public:
-    int strStr(string haystack, string needle) {
-        int ans = -1;
-        string temp = needle;
-        for (int i = 0; i < haystack.size(); i++) {
-            cout<<temp<<endl;
-            if (haystack[i] == temp[0]) {
-                ans = i;
-                temp = temp.substr(1, temp.size()-1);
+    int strStr(const string& haystack, const string& needle) {
+        int hSize = haystack.size();
+        int nSize = needle.size();
+
+        
+        if (nSize == 0) return 0;
+        
+        if (nSize > hSize) return -1;
+
+        
+        vector<int> lps = buildLPS(needle);
+
+        int i = 0, j = 0;  
+        while (i < hSize) {
+            if (haystack[i] == needle[j]) {
+                i++;
+                j++;
+                
+                if (j == nSize) return i - j;
+            } else if (j > 0) {
+                j = lps[j - 1];  
             } else {
-                temp = needle;
-                ans = -1;
-            }
-            if (temp.size() == 0) {
-                return i-needle.size()+1;
+                i++;  
             }
         }
-        if(temp.size()!=0){
-            return -1;
+
+        return -1; 
+    }
+
+private:
+    // Helper function to build the LPS array.
+    vector<int> buildLPS(const string& needle) {
+        int nSize = needle.size();
+        vector<int> lps(nSize, 0);
+        int len = 0;  
+
+        for (int i = 1; i < nSize; ) {
+            if (needle[i] == needle[len]) {
+                lps[i++] = ++len;
+            } else if (len > 0) {
+                len = lps[len - 1]; 
+            } else {
+                lps[i++] = 0;  
+            }
         }
-        return ans;
+        return lps;
     }
 };
