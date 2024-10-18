51462f59bd4f36617cd38593d8687f58ced3ff6b
nonsk <132883762+nonsk@users.noreply.github.com>
2024-10-18 23:10:24 +0530

    Time: 55 ms (99.32%), Space: 75.4 MB (20.94%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set <int> s;
+        for(auto it:nums){
+            s.insert(it);
+        }
+        if(s.size()==nums.size()){return false;}
+        return true;
+    }
+};
