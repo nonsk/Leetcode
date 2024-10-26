92bcad0198ef588f4d76d92b6687b23fc4d91303
nonsk <132883762+nonsk@users.noreply.github.com>
2024-10-26 20:36:45 +0530

    Time: 35 ms (26.08%), Space: 42.9 MB (26.33%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -4,7 +4,7 @@ public:
         int m = s.size();
         int n = t.size();
         
-        // Use long long for dp to prevent overflow
+        const int MOD = 1e9 + 7;
         vector<vector<long long>> dp(n+1, vector<long long>(m+1, 0));
         
         // Base case: An empty string `t` is a subsequence of any prefix of `s`
@@ -16,7 +16,7 @@ public:
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
                 if(s[j-1] == t[i-1]) {
-                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+                    dp[i][j] = (dp[i-1][j-1] + dp[i][j-1]) % MOD;
                 } else {
                     dp[i][j] = dp[i][j-1];
                 }
