d213bef0adab53ab2f8aadf97138438be50135d5
nonsk <132883762+nonsk@users.noreply.github.com>
2024-10-26 18:58:50 +0530

    Time: 3 ms (97.6%), Space: 27.5 MB (38.51%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -1,18 +1,23 @@
-
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-        if (preorder.empty()) {
+    unordered_map<int, int> inorder_map;
+
+    TreeNode* solve(vector<int>& preorder, int pre_start, int pre_end, int in_start, int in_end) {
+        if (pre_start > pre_end || in_start > in_end) {
             return nullptr;
         }
-        TreeNode* ans = new TreeNode(preorder[0]);
-        int root_index = find(inorder.begin(), inorder.end(), preorder[0]) - inorder.begin();
-        vector<int> left_inorder(inorder.begin(), inorder.begin() + root_index);
-        vector<int> right_inorder(inorder.begin() + root_index + 1, inorder.end());
-        vector<int> left_preorder(preorder.begin() + 1, preorder.begin() + 1 + left_inorder.size());
-        vector<int> right_preorder(preorder.begin() + 1 + left_inorder.size(), preorder.end());
-        ans->left = buildTree(left_preorder, left_inorder);
-        ans->right = buildTree(right_preorder, right_inorder);
-        return ans;
+        int root_val = preorder[pre_start];
+        TreeNode* root = new TreeNode(root_val);
+        int root_index = inorder_map[root_val];
+        int left_size = root_index - in_start;
+        root->left = solve(preorder, pre_start + 1, pre_start + left_size, in_start, root_index - 1);
+        root->right = solve(preorder, pre_start + left_size + 1, pre_end, root_index + 1, in_end);
+        return root;
+    }
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        for (int i = 0; i < inorder.size(); i++) {
+            inorder_map[inorder[i]] = i;
+        }
+        return solve(preorder, 0, preorder.size() - 1, 0, inorder.size() - 1);
     }
 };
