e758d8c2887dd0a303af8dc1ee960c02466ebd4a
nonsk <132883762+nonsk@users.noreply.github.com>
2024-10-28 04:21:32 +0530

    Time: 83 ms (6.8%), Space: 36.4 MB (5.44%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+    unordered_map<TreeNode*, unordered_map<bool, int>> dp;
+    int solve(bool can, TreeNode* root) {
+        if (dp[root].count(can)) {
+            return dp[root][can];
+        }
+        if (root == NULL) {
+            dp[root][can] = 0;
+            return 0;
+        }
+        cout << root->val << " ";
+        if (!can) {
+            dp[root][can] = solve(true, root->left) + solve(true, root->right);
+            return dp[root][can];
+        }
+        dp[root][can] = max(root->val + solve(false, root->left) +
+                                solve(false, root->right),
+                            solve(true, root->left) + solve(true, root->right));
+        return dp[root][can];
+    }
+    int rob(TreeNode* root) { return solve(true, root); }
+};
