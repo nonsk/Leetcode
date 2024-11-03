cad45be55366ea6c6300202b58cad71d811c4c72
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-03 15:19:20 +0530

    Time: 45 ms (26.78%), Space: 28.6 MB (20.79%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -1,10 +1,7 @@
 class Solution {
 public:
-    int dfs(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+    int dfs(int i, int j, vector<vector<int>>& matrix, vector<vector<int>>& dp, int &n, int &m) {
         if (dp[i][j] != -1) return dp[i][j];
-        
-        int n = matrix.size();
-        int m = matrix[0].size();
         int maxPath = 1;
         
         vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
@@ -14,7 +11,7 @@ public:
             int nj = j + dir.second;
             
             if (ni >= 0 && ni < n && nj >= 0 && nj < m && matrix[ni][nj] > matrix[i][j]) {
-                maxPath = max(maxPath, 1 + dfs(ni, nj, matrix, dp));
+                maxPath = max(maxPath, 1 + dfs(ni, nj, matrix, dp,n,m));
             }
         }
         
@@ -32,7 +29,7 @@ public:
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                answer = max(answer, dfs(i, j, matrix, dp));
+                answer = max(answer, dfs(i, j, matrix, dp,n,m));
             }
         }
         
