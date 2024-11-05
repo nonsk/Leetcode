fe6a45b609b4990894703e5e473f3a599a2ad99e
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-05 21:24:38 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        stack<int> st;
+        int n = nums.size();
+        vector<int> ans(n, -1);
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums[i]) {
+                st.pop();
+            }
+            st.push(nums[i]);
+            stack<int> st2 = st;
+            while (!st2.empty()) {
+                cout << st2.top() << " ";
+                st2.pop();
+            }
+            cout << endl;
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.empty() && st.top() <= nums[i]) {
+                st.pop();
+            }
+            if (st.empty()) {
+                ans[i] = -1;
+            } else {
+                ans[i] = st.top();
+            }
+                st.push(nums[i]);
+            stack<int> st2 = st;
+            while (!st2.empty()) {
+                cout << st2.top() << " ";
+                st2.pop();
+            }
+            cout << endl;
+        }
+        return ans;
+    }
+};
