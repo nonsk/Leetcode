cfde215392f2dd584e468c89f241425dc07c4628
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-09 18:18:42 +0530

    Time: 27 ms (98.92%), Space: 11 MB (95.14%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,1 +1,24 @@
-class Solution {public:int mod=1e9+7;int solve(vector<int>&groups,vector<int>&profit,int i,int p,int n,int&minProfit,vector<vector<vector<int>>>&dp){if(i==profit.size()||n==0){if(p>=minProfit)return 1;return 0;}if(dp[i][n][min(p,minProfit)]!=-1)return dp[i][n][min(p,minProfit)];long long int exc=solve(groups,profit,i+1,p,n,minProfit,dp);long long int inc=0;if(groups[i]<=n)inc=solve(groups,profit,i+1,profit[i]+p,n-groups[i],minProfit,dp);return dp[i][n][min(p,minProfit)]=(inc%mod+exc%mod)%mod;}int profitableSchemes(int n,int minProfit,vector<int>&group,vector<int>&profit){int size=profit.size();vector<vector<vector<int>>>dp(size,vector<vector<int>>(n+1,vector<int>(minProfit+1,-1)));return solve(group,profit,0,0,n,minProfit,dp);}};
+class Solution {
+public:
+    int mod = 1e9 + 7;
+
+    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        vector<vector<int>> dp(n + 1, vector<int>(minProfit + 1, 0));
+        dp[0][0] = 1;
+        for (int i = 0; i < group.size(); i++) {
+            int members = group[i];
+            int profitGain = profit[i];
+            for (int j = n; j >= members; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    int newProfit = min(k + profitGain, minProfit);
+                    dp[j][newProfit] = (dp[j][newProfit] + dp[j - members][k]) % mod;
+                }
+            }
+        }
+        int totalSchemes = 0;
+        for (int j = 0; j <= n; j++) {
+            totalSchemes = (totalSchemes + dp[j][minProfit]) % mod;
+        }
+        return totalSchemes;
+    }
+};
