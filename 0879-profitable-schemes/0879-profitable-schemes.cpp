183f4410dc6451c0e7fb7e85830d39f67078ee5a
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-09 18:12:19 +0530

    Time: 195 ms (22.16%), Space: 55.1 MB (44.83%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.cpp
+++ 0879-profitable-schemes/0879-profitable-schemes.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
-    int solve(vector<int>&groups, vector<int>&profit , int i ,int p, int n ,int &minProfit , vector<vector<int>>&dp )
+    int solve(vector<int>&groups, vector<int>&profit , int i ,int p, int n ,int &minProfit , vector<vector<vector<int>>>&dp )
     {
         if(i == profit.size() || n == 0)
         {
@@ -9,19 +9,21 @@ public:
                 return 1;
             return 0;    
         }
-        if (dp[i][n] != -1)
-            return dp[i][n];
+        if (dp[i][n][min(p, minProfit)] != -1)
+            return dp[i][n][min(p, minProfit)];
             
-        long long exc = solve(groups ,profit , i+1 , p ,  n , minProfit ,dp);
-        long long inc = 0 ;
+        long long int exc = solve(groups ,profit , i+1 , p ,  n , minProfit ,dp);
+        long long int inc = 0 ;
         
         if(groups[i] <= n )
             inc = solve(groups, profit , i+1 , profit[i]+p  , n-groups[i] , minProfit,dp);
-         return dp[i][n] = (inc % mod + exc % mod) % mod;
+         return dp[i][n][min(p, minProfit)] = (inc % mod + exc % mod) % mod;
     }
     int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
+        
         int size = profit.size();
-        vector<vector<int>>dp( size , vector<int>(n+1,-1));
+
+        vector<vector<vector<int>>>dp( size , vector<vector<int>>(n+1,vector<int>(minProfit+1,-1)));
         return solve(group, profit , 0 , 0 , n, minProfit , dp);
     }
 };
