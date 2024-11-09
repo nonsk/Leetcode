9aa587a5638751e929791f381bb37d895aaedd8e
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-09 17:11:38 +0530

    Time: 2 ms (32.08%), Space: 13.1 MB (16.18%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int solve(int day, vector<int>& days, vector<int>& costs, vector<int>& dp, unordered_set<int>& travelDays) {
+        if (day > 365) {
+            return 0;
+        }
+        if (dp[day] != -1) {
+            return dp[day];
+        }
+        if (travelDays.find(day) == travelDays.end()) {
+            return dp[day] = solve(day + 1, days, costs, dp, travelDays);
+        }
+        int ans = min({
+            solve(day + 1, days, costs, dp, travelDays) + costs[0],
+            solve(day + 7, days, costs, dp, travelDays) + costs[1],
+            solve(day + 30, days, costs, dp, travelDays) + costs[2]
+        });
+        dp[day] = ans;
+        return ans;
+    }
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int> dp(366, -1);
+        unordered_set<int> travelDays(days.begin(), days.end());
+        return solve(0, days, costs, dp, travelDays);
+    }
+};
