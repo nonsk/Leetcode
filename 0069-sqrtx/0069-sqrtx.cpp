c386da04ebd16e7ffa658b82ae1d939aa6a5cd3b
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-16 22:26:23 +0530

    Time: 0 ms (100%), Space: 8.4 MB (18.79%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
     int mySqrt(int x) {
-         ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-        if(x<2) return x;
-        long l=0,r=x-1,ans=0;
-        while(l<=r)
-        {
-            long m=l+(r-l)/2;
-
-            if(m*m==x)
-            {
-                return m;
-            }
-            else if(m*m<x)
-            {
-                ans=m;
-                l=m+1;
-            }
-            else{
-                r=m-1;
+        if (x == 0 || x == 1) return x;
+        int left = 1, right = x, result = 0;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (mid <= x / mid) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-            return ans;        
+        return result;
     }
 };
