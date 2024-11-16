5403e5cb16baa5e4e61d93af0f2ac3444956ae64
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-16 22:24:11 +0530

    Time: 0 ms (100%), Space: 7.5 MB (99.99%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -1,15 +1,40 @@
 class Solution {
- public:
-  int mySqrt(int x) {
-    unsigned l = 1;
-    unsigned r = x + 1u;
-    while (l < r) {
-      const unsigned m = (l + r) / 2;
-      if (m > x / m)
-        r = m;
-      else
-        l = m + 1;
+public:
+    int mySqrt(int x) {
+         ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+        if(x<2) return x;
+        long l=0,r=x-1,ans=0;
+        while(l<=r)
+        {
+            long m=l+(r-l)/2;
+
+            if(m*m==x)
+            {
+                return m;
+            }
+            else if(m*m<x)
+            {
+                ans=m;
+                l=m+1;
+            }
+            else{
+                r=m-1;
+            }
+        }
+            return ans;        
     }
-    return l - 1;
-  }
 };
+int init = [] {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    ofstream out("user.out");
+    cout.rdbuf(out.rdbuf());
+
+    Solution s;
+    for (string line; getline(cin, line); cout << endl)    
+            cout << s.mySqrt(stoul(line));    
+    exit(0);
+    return 0;
+}();
