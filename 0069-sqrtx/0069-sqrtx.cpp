4f03cc3d001a8194d96250ce6bfe7529962d2e26
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-16 22:23:32 +0530

    Time: 0 ms (100%), Space: 8.1 MB (98.17%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,7 +3,6 @@ class Solution {
   int mySqrt(int x) {
     unsigned l = 1;
     unsigned r = x + 1u;
-
     while (l < r) {
       const unsigned m = (l + r) / 2;
       if (m > x / m)
@@ -11,8 +10,6 @@ class Solution {
       else
         l = m + 1;
     }
-
-    // l := the minimum number s.t. l * l > x
     return l - 1;
   }
 };
