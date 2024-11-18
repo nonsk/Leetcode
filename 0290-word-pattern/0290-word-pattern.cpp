f23397c9e7b8575c8615a1f8d61cd8d77a57bcd3
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-19 01:38:30 +0530

    Time: 2 ms (15.59%), Space: 8.6 MB (7.8%) - LeetHub

---
+++ 0290-word-pattern/0290-word-pattern.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    bool wordPattern(string pattern, string s) {
+        string current = "";
+        vector<string> yo;
+        for (auto c : s) {
+            if (c == ' ') {
+                yo.push_back(current);
+                current = "";
+            } else {
+                current += c;
+            }
+        }
+        if (current != "") {
+            yo.push_back(current);
+        }
+        for (auto it : yo) {
+            cout << it << endl;
+        }
+        unordered_set<char> st1;
+        unordered_set<string> st2;
+        for (auto c : pattern) {
+            st1.insert(c);
+        }
+        for (auto c : yo) {
+            st2.insert(c);
+        }
+        for (auto it : st1) {
+            cout << it << endl;
+        }
+        for (auto it : st2) {
+            cout << it << endl;
+        }
+        if (pattern.size() != yo.size()) {
+            return false;
+        }
+        unordered_map<char, string> charToWord;
+        unordered_map<string, char> wordToChar;
+        for (size_t i = 0; i < pattern.size(); ++i) {
+            char c = pattern[i];
+            string word = yo[i];
+            if (charToWord.find(c) != charToWord.end()) {
+                if (charToWord[c] != word) {
+                    return false;
+                }
+            } else {
+                charToWord[c] = word;
+            }
+            if (wordToChar.find(word) != wordToChar.end()) {
+                if (wordToChar[word] != c) {
+                    return false;
+                }
+            } else {
+                wordToChar[word] = c;
+            }
+        }
+        return true;
+    }
+};
