614c6720ee36515a24f6288bd99db17ffee1354e
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-18 22:45:48 +0530

    Time: 3 ms (42.62%), Space: 114.5 MB (61.81%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -4,14 +4,11 @@ public:
       int minimum = INT_MAX;
       int s_minimum = INT_MAX;
       for(int i =0; i<nums.size(); i++){
-        if(nums[i]<s_minimum){
-            if(nums[i]<minimum){
-                minimum = nums[i];
-                s_minimum = nums[i];
-            }
-            else {
-                s_minimum = nums[i];
-            }
+        if(nums[i]<=minimum){
+            minimum = nums[i];
+        }
+        else if (nums[i]<=s_minimum){
+            s_minimum = nums[i];
         }
         else{
             return true;
