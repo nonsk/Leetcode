5af7c967f7eff21c0e6c8a23a9f1e9b2f22673be
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-20 22:34:04 +0530

    Time: 0 ms (100%), Space: 12.2 MB (56.49%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,21 +11,12 @@
  */
 class Solution {
 public:
-struct TreeNode {
-    int val;
-    TreeNode* left;
-    TreeNode* right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {}
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-    TreeNode(int x, TreeNode* left, TreeNode* right) : val(x), left(left), right(right) {}
-};
-
-bool isSameTree(TreeNode* p, TreeNode* q) {
-    if (!p && !q) return true;       // Both nodes are null
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+         if (!p && !q) return true;       // Both nodes are null
     if (!p || !q) return false;      // One node is null and the other is not
     if (p->val != q->val) return false; // Node values are different
     
     // Recursively check left and right subtrees
     return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
-}
+    }
 };
