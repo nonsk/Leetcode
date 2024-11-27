c88dd0bff47ea47214f433163bd77a6277eee9ac
nonsk <132883762+nonsk@users.noreply.github.com>
2024-11-27 23:59:20 +0530

    Time: 71 ms (54.02%), Space: 94.8 MB (74.19%) - LeetHub

---
+++ 2542-maximum-subsequence-score/2542-maximum-subsequence-score.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    long long maxScore(vector<int>& nums1, vector<int>& nums2, int k) {
+        int n = nums1.size();
+        vector<pair<int,int>> pairs(n);
+
+        for(int i =0; i<n; i++){
+            pairs[i] = {nums2[i], nums1[i]};
+        }
+
+        sort(pairs.rbegin(), pairs.rend());
+        priority_queue<int, vector<int>, greater<int>> minHeap;
+        long long sum = 0, maxScore = 0;
+
+        for(auto & [minNum, num]:pairs){
+            sum+=num;
+            minHeap.push(num);
+
+            if(minHeap.size()>k){
+                sum-=minHeap.top();
+                minHeap.pop();
+            }
+            if(minHeap.size()==k){
+                maxScore = max(maxScore, sum*minNum);
+            }
+        }
+
+        return maxScore;
+    }
+};
